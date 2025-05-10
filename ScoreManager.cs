55e5fb03a3d7fba6bddbd2ec32d6e2ccfd6c31a5
sophiavasylyeva <122943267+sophiavasylyeva@users.noreply.github.com>
2025-05-10 12:32:37 -0500

    Snow scene

--- ScoreManager.cs
+++ ScoreManager.cs
@@ -1,22 +1,3 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-
-// public class ScoreManager : MonoBehaviour
-// {
-//     // Start is called before the first frame update
-//     void Start()
-//     {
-        
-//     }
-
-//     // Update is called once per frame
-//     void Update()
-//     {
-        
-//     }
-// }
-
 using TMPro;
 using UnityEngine;
 
