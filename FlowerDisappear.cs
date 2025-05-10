9d875cc580fa6983bc17fdf075803832dcfc00c3
sophiavasylyeva <122943267+sophiavasylyeva@users.noreply.github.com>
2025-05-10 12:36:37 -0500

    Add files via upload

---
+++ FlowerDisappear.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class flower01 : MonoBehaviour
+{
+    private XRBaseInteractable interactable;
+
+    private void Awake()
+    {
+        interactable = GetComponent<XRBaseInteractable>();
+        interactable.selectEntered.AddListener(OnSelectEntered);
+    }
+
+    private void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        Destroy(gameObject); // Make the flower disappear
+    }
+
+    private void OnDestroy()
+    {
+        // Cleanup to avoid memory leaks
+        if (interactable != null)
+            interactable.selectEntered.RemoveListener(OnSelectEntered);
+    }
+}
